c4d9c17ebd8b9e8ecdf432748a877227e0c7d7e4
Josh A <joshmail9510@gmail.com>
2021-06-15 09:08:58 -0400

    Deployment to heroku

---
+++ Appointments-Express-Backend/Migrations/20210615062113_postgres.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Appointments_Express_Backend.Migrations
+{
+    public partial class postgres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
