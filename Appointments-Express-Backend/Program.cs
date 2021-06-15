2f99a7a910235c69bb0b456e90eeac189f90a3ce
Josh A <joshmail9510@gmail.com>
2021-06-15 09:53:21 -0400

    testing

--- Appointments-Express-Backend/Program.cs
+++ Appointments-Express-Backend/Program.cs
@@ -13,7 +13,7 @@ namespace Appointments_Express_Backend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
