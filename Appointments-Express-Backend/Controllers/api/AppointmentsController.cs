61a3c8e1715c81004e6e6460d418e135a715fad4
Josh A <joshmail9510@gmail.com>
2021-09-21 23:08:49 -0400

    Appointments Controller

---
+++ Appointments-Express-Backend/Controllers/api/AppointmentsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Appointments_Express_Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Appointments_Express_Backend.DTO.Requests;
+
+namespace Appointments_Express_Backend.Controllers.api
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly AppointmentDBContext _context;
+
+        public AppointmentsController(AppointmentDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Appointments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointments()
+        {
+            return await _context.Appointments.ToListAsync();
+        }
+
+        // GET: api/Appointments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appointment>> GetAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return appointment;
+        }
+
+        // POST: api/Appointments
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost("{storeId}/{customerId}")]
+        public async Task<ActionResult<Appointment>> PostAppointment([FromRoute] int storeId, [FromRoute] int customerId, [FromBody] Appointment appointment)
+        {
+            appointment.customerId = customerId;
+            appointment.storeId = storeId;
+            _context.Appointments.Add(appointment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAppointment", new { id = appointment.id }, appointment);
+        }
+
+        // DELETE: api/Appointments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Appointment>> DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Appointments.Remove(appointment);
+            await _context.SaveChangesAsync();
+
+            return appointment;
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return _context.Appointments.Any(e => e.id == id);
+        }
+    }
+}
